b5b3b7e26456f0ea934bd37a3e74344e6a930d76
andrewjholbrook <aholbroo@g.ucla.edu>
2023-07-17 11:58:37 -0700

    another try

--- code/create_xml.py
+++ code/create_xml.py
@@ -6,7 +6,7 @@ def main():
     for steps in [4,8,16,32,64]:
         for rates in [0.2,0.3,0.4,0.5,0.6,0.7,0.8]:
             with open("mcmc_handbook/code/tuning.xml","r") as infile:
-                with open("mcmc_handbook/code/tuning_" +str(steps) +"_" + str(rates) +".xml","w") as outfile:
+                with open("mcmc_handbook/code/tuning" +str(steps)  + str(rates) +".xml","w") as outfile:
                     for s in infile:
                         outfile.write(s.replace("nSteps=\"4\"", "nSteps=\"" + str(steps) + "\"").replace("targetAcceptanceProbability=\"0.45\"", "targetAcceptanceProbability=\"" +  str(rates) + "\"").replace("tuning.log","tuning"+str(steps)+str(rates)+".log"))
 
