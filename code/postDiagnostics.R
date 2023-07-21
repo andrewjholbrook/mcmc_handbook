6e4059613bee41dc5e6aed1d70b5611630aa97cf
andrewjholbrook <aholbroo@g.ucla.edu>
2023-07-21 09:43:45 -0700

    ctmc performance comparison

--- code/postDiagnostics.R
+++ code/postDiagnostics.R
@@ -10,7 +10,7 @@ df <- read_table2("output/withHmc.log", skip = 3)
 df <- df[df$state>=1000,12:1903]
 effs <- effectiveSize(df)
 
-df2 <- read_table2("output/noHmc.log", skip = 3)
+df2 <- read_table2("output/noHmc2.log", skip = 3)
 df2 <- df2[df2$state>=1000,12:1903]
 effs2 <- effectiveSize(df2)
 
@@ -19,22 +19,31 @@ df4 <- data.frame(Values=effs2,Algorithm="RW")
 
 df <- rbind(df3,df4)
 
+# copy paste hours from timing files
+hmcHours <- 19.033439722222223
+noHmcHours <- 20.454540833333333
+
+df$Values[df$Algorithm=="HMC"] <- df$Values[df$Algorithm=="HMC"] / 19.033439722222223
+df$Values[df$Algorithm=="RW"] <- df$Values[df$Algorithm=="RW"] / 20.454540833333333
+
+df$Algorithm[df$Algorithm=="HMC"] <- "Surrogate-trajectory\nHMC"
+df$Algorithm[df$Algorithm=="RW"] <- "Random walk"
 
 
 gg <- ggplot(df,aes(x=Values, fill=Algorithm)) +
-  geom_histogram() + ggtitle("Median effective sample size") +
-  #scale_fill_continuous_sequential(palette = "Heat2")+
-  theme_bw()
+  geom_histogram() + ggtitle(NULL) +
+  scale_x_log10() +
+  xlab("Effective sample size per hour") +
+  ylab("Number of parameters") +
+  scale_fill_discrete_qualitative(palette = "Harmonic") +
+  theme_bw() 
 gg
 
 
-
-
-# ggsave("figures/ctmcTuning.pdf",ggarrange(gg,NULL,gg2,nrow=1,labels=c("a","","b"),
-#                                           widths = c(1, -0.01, 1)),width = 10,height=4)
-# system2(command = "pdfcrop",
-#         args    = c("~/mcmc_handbook/figures/ctmcTuning.pdf",
-#                     "~/mcmc_handbook/figures/ctmcTuning.pdf")
-# )
+ggsave("figures/ctmcPerformance.pdf",gg,width = 6,height=2.4)
+system2(command = "pdfcrop",
+        args    = c("~/mcmc_handbook/figures/ctmcPerformance.pdf",
+                    "~/mcmc_handbook/figures/ctmcPerformance.pdf")
+)
 
 
